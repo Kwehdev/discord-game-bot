9d5383fb1adca1957d4802cc853dc5b1c2fb467c
KwehDev <KwehDev@protonmail.com>
2021-03-01 15:09:12 +0200

    feat: add bot connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World')
+import { Client, Message } from 'discord.js'
+
+const client = new Client()
+const COMMAND_PREFIX = '?'
+
+client.on('ready', () => {
+  if (client.user) {
+    console.log(`${new Date()}: Logged in as ${client.user.tag}`)
+  }
+})
+
+client.on('message', (msg: Message) => {
+  // Return if MSG is not a command, or if author is bot.
+  if (!msg.content.startsWith(COMMAND_PREFIX) || msg.author.bot) return
+
+  const [command, ...args] = msg.content.slice(COMMAND_PREFIX.length).split(' ')
+
+  switch (command.toLowerCase()) {
+    default:
+      return msg.channel.send('Unknown command.')
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
